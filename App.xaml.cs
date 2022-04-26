c8095c588e01b855ad5c2ce365a3b832bdd5adae
hrtk91 <ht91218@gmail.com>
2022-04-26 21:12:23 +0900

    エラー時にログが出るように変更

--- App.xaml.cs
+++ App.xaml.cs
@@ -4,6 +4,7 @@ using System.Management;
 using System.Windows;
 using System.Windows.Forms;
 using System.Windows.Threading;
+using NLog;
 
 namespace CPUMEM
 {
@@ -15,6 +16,8 @@ namespace CPUMEM
         private DispatcherTimer Timer { get; } = new DispatcherTimer();
 
         private NotifyIcon Icon { get; } = new NotifyIcon();
+
+        private ILogger logger { get; } = NLog.LogManager.GetCurrentClassLogger();
 
         protected override void OnStartup(StartupEventArgs e)
         {
@@ -44,51 +47,67 @@ namespace CPUMEM
 
         private void ClickExit(object? sender, EventArgs e)
         {
-            Timer.Stop();
-            System.Windows.Application.Current.Shutdown();
+            try
+            {
+                Timer.Stop();
+                System.Windows.Application.Current.Shutdown();
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, $"{nameof(ClickExit)} : 例外が発生");
+                throw;
+            }
         }
 
         private void Tick(object? sender, EventArgs e)
         {
-            using (var mc = new ManagementClass("Win32_OperatingSystem"))
-            using (var moc = mc.GetInstances())
-            using (var mc2 = new ManagementClass("Win32_Processor"))
-            using (var moc2 = mc2.GetInstances())
+            try
             {
-                var cpuload = 0.0f;
-                foreach (var mo2 in moc2)
+                using (var mc = new ManagementClass("Win32_OperatingSystem"))
+                using (var moc = mc.GetInstances())
+                using (var mc2 = new ManagementClass("Win32_Processor"))
+                using (var moc2 = mc2.GetInstances())
                 {
-                    var load = (UInt16)mo2["LoadPercentage"];
-                    cpuload = (float)load / 100f;
-                    mo2.Dispose();
+                    var cpuload = 0.0f;
+                    foreach (var mo2 in moc2)
+                    {
+                        var load = (UInt16)mo2["LoadPercentage"];
+                        cpuload = (float)load / 100f;
+                        mo2.Dispose();
+                    }
+
+                    var memload = 0.0f;
+                    foreach (ManagementObject mo in moc)
+                    {
+                        var total = (UInt64)mo["TotalVisibleMemorySize"];
+                        var current = (UInt64)mo["FreePhysicalMemory"];
+                        memload = (float)(total - current) / (float)total;
+                        Icon.Text =$"CPU: {(cpuload * 100).ToString("00")}%\r\nMem: {(memload * 100).ToString("00")}%";
+
+                        mo.Dispose();
+                    }
+
+                    var bitmap = new Bitmap(32, 32);
+                    using (Graphics g = Graphics.FromImage(bitmap))
+                    {
+                        // 枠の描画
+                        g.DrawRectangle(new Pen(Color.Gray), new Rectangle(0, 0, bitmap.Width - 1, bitmap.Height - 1));
+                        
+                        // CPU使用率の描画
+                        g.FillRectangle(Brushes.Aqua,  new Rectangle(4, 32 - (int)(bitmap.Height * cpuload), 10, (int)(bitmap.Height * cpuload)));
+
+                        // メモリ使用率の描画
+                        g.FillRectangle(Brushes.Orange,  new Rectangle(18, 32 - (int)(bitmap.Height * memload), 10, (int)(bitmap.Height * memload)));
+                    }
+
+                    // アイコンに反映
+                    Icon.Icon = System.Drawing.Icon.FromHandle(bitmap.GetHicon());
                 }
-
-                var memload = 0.0f;
-                foreach (ManagementObject mo in moc)
-                {
-                    var total = (UInt64)mo["TotalVisibleMemorySize"];
-                    var current = (UInt64)mo["FreePhysicalMemory"];
-                    memload = (float)(total - current) / (float)total;
-                    Icon.Text =$"CPU: {(cpuload * 100).ToString("00")}%\r\nMem: {(memload * 100).ToString("00")}%";
-
-                    mo.Dispose();
-                }
-
-                var bitmap = new Bitmap(32, 32);
-                using (Graphics g = Graphics.FromImage(bitmap))
-                {
-                    // 枠の描画
-                    g.DrawRectangle(new Pen(Color.Gray), new Rectangle(0, 0, bitmap.Width - 1, bitmap.Height - 1));
-                    
-                    // CPU使用率の描画
-                    g.FillRectangle(Brushes.Aqua,  new Rectangle(4, 32 - (int)(bitmap.Height * cpuload), 10, (int)(bitmap.Height * cpuload)));
-
-                    // メモリ使用率の描画
-                    g.FillRectangle(Brushes.Orange,  new Rectangle(18, 32 - (int)(bitmap.Height * memload), 10, (int)(bitmap.Height * memload)));
-                }
-
-                // アイコンに反映
-                Icon.Icon = System.Drawing.Icon.FromHandle(bitmap.GetHicon());
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, $"{nameof(Tick)} : 例外が発生");
+                throw;
             }
         }
     }
