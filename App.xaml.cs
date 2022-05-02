1d4c86655945af7f95e4d0eea21dc527dd585d5c
hrtk91 <ht91218@gmail.com>
2022-05-03 06:54:06 +0900

    例外キャッチを細分化

--- App.xaml.cs
+++ App.xaml.cs
@@ -55,7 +55,6 @@ namespace CPUMEM
             catch (Exception ex)
             {
                 logger.Error(ex, $"{nameof(ClickExit)} : 例外が発生");
-                throw;
             }
         }
 
@@ -63,50 +62,125 @@ namespace CPUMEM
         {
             try
             {
-                using (var mc = new ManagementClass("Win32_OperatingSystem"))
-                using (var moc = mc.GetInstances())
-                using (var mc2 = new ManagementClass("Win32_Processor"))
-                using (var moc2 = mc2.GetInstances())
+                var cpuload = GetCpuLoad();
+                var memload = GetMemoryLoad();
+
+                UpdateTooltip(cpuload, memload);
+
+                using (var bmp = CreateIcon(cpuload, memload))
                 {
-                    var cpuload = 0.0f;
-                    foreach (var mo2 in moc2)
-                    {
-                        var load = (UInt16)mo2["LoadPercentage"];
-                        cpuload = (float)load / 100f;
-                        mo2.Dispose();
-                    }
-
-                    var memload = 0.0f;
-                    foreach (ManagementObject mo in moc)
-                    {
-                        var total = (UInt64)mo["TotalVisibleMemorySize"];
-                        var current = (UInt64)mo["FreePhysicalMemory"];
-                        memload = (float)(total - current) / (float)total;
-                        Icon.Text =$"CPU: {(cpuload * 100).ToString("00")}%\r\nMem: {(memload * 100).ToString("00")}%";
-
-                        mo.Dispose();
-                    }
-
-                    var bitmap = new Bitmap(32, 32);
-                    using (Graphics g = Graphics.FromImage(bitmap))
-                    {
-                        // 枠の描画
-                        g.DrawRectangle(new Pen(Color.Gray), new Rectangle(0, 0, bitmap.Width - 1, bitmap.Height - 1));
-                        
-                        // CPU使用率の描画
-                        g.FillRectangle(Brushes.Aqua,  new Rectangle(4, 32 - (int)(bitmap.Height * cpuload), 10, (int)(bitmap.Height * cpuload)));
-
-                        // メモリ使用率の描画
-                        g.FillRectangle(Brushes.Orange,  new Rectangle(18, 32 - (int)(bitmap.Height * memload), 10, (int)(bitmap.Height * memload)));
-                    }
-
-                    // アイコンに反映
-                    Icon.Icon = System.Drawing.Icon.FromHandle(bitmap.GetHicon());
+                    UpdateIcon(bmp);
                 }
             }
             catch (Exception ex)
             {
                 logger.Error(ex, $"{nameof(Tick)} : 例外が発生");
+            }
+        }
+
+        private float GetCpuLoad()
+        {
+            try
+            {
+                using (var mc = new ManagementClass("Win32_Processor"))
+                using (var moc = mc.GetInstances())
+                {
+                    var cpuload = 0.0f;
+                    if (moc is not null)
+                    {
+                        foreach (var mo2 in moc)
+                        {
+                            var load = (ushort)(mo2["LoadPercentage"] ?? (ushort)0);
+                            cpuload = (load is (ushort)0) ? 0.0f : (float)load / 100f;
+                            mo2.Dispose();
+                        }
+                    }
+                    return cpuload;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("CPU処理負荷の取得に失敗しました。", ex);
+            }
+        }
+
+        private float GetMemoryLoad()
+        {
+            try
+            {
+                using (var mc = new ManagementClass("Win32_OperatingSystem"))
+                using (var moc = mc.GetInstances())
+                {
+                    var memload = 0.0f;
+                    if (moc is not null)
+                    {
+                        foreach (ManagementObject mo in moc)
+                        {
+                            var total = (ulong)(mo["TotalVisibleMemorySize"] ?? 0ul);
+                            var current = (ulong)(mo["FreePhysicalMemory"] ?? 0ul);
+
+                            if (total is not 0ul)
+                            {
+                                memload = (float)(total - current) / (float)total;
+                            }
+
+                            mo.Dispose();
+                        }
+                    }
+                    return memload;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Memory処理負荷の取得に失敗しました。", ex);
+            }
+        }
+
+        private void UpdateTooltip(float cpuload, float memload)
+        {
+            try
+            {
+                Icon.Text =$"CPU: {(cpuload * 100).ToString("00")}%\r\nMem: {(memload * 100).ToString("00")}%";
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("ToolTipの更新処理に失敗しました。", ex);
+            }
+        }
+
+        private Bitmap CreateIcon(float cpuload, float memload)
+        {
+            try
+            {
+                var bitmap = new Bitmap(32, 32);
+                using (Graphics g = Graphics.FromImage(bitmap))
+                {
+                    // 枠の描画
+                    g.DrawRectangle(new Pen(Color.Gray), new Rectangle(0, 0, bitmap.Width - 1, bitmap.Height - 1));
+                    
+                    // CPU使用率の描画
+                    g.FillRectangle(Brushes.Aqua,  new Rectangle(4, 32 - (int)(bitmap.Height * cpuload), 10, (int)(bitmap.Height * cpuload)));
+
+                    // メモリ使用率の描画
+                    g.FillRectangle(Brushes.Orange,  new Rectangle(18, 32 - (int)(bitmap.Height * memload), 10, (int)(bitmap.Height * memload)));
+                }
+                return bitmap;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("アイコンの作成処理に失敗しました。", ex);
+            }
+        }
+
+        private void UpdateIcon(Bitmap bmp)
+        {
+            try
+            {
+                Icon.Icon = System.Drawing.Icon.FromHandle(bmp.GetHicon());
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("アイコンの更新処理に失敗しました。", ex);
             }
         }
     }
